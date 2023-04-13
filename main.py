9cca6967bfcc17cb7c06d0066381d616ff258dbf
swalha1999 <mohamd514.m@gmail.com>
2023-04-13 10:01:51 +0300

    added index.html and modified main.py

--- main.py
+++ main.py
@@ -6,11 +6,21 @@ import chess.pgn
 
 pgn_file = open("game.pgn")
 game = chess.pgn.read_game(pgn_file)
-board = game.board()
-sf = Stockfish("stockfish_20011801_x64.exe")
+
+board = game.board() # get the board from the game object
+
+white_player = game.headers["White"] # get the white player name
+black_player = game.headers["Black"] # get the black player name
+
+
+sf = Stockfish("stockfish-windows-2022-x86-64-avx2.exe")
 # we can access the moves in the game using the mainline_moves() method
 # we get an array of moves and we can iterate over it
 
 for move in game.mainline_moves():
+    # we can use the push() method to make the move on the board
+    board.push(move)
+
     print(move)
 
+print(white_player, "vs", black_player)
