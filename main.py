732e26b0ede64653418a281d5154356ba3efda16
swalha1999 <mohamd514.m@gmail.com>
2023-09-06 00:31:56 +0300

    multi game prossing and user graoth

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from alive_progress import alive_it
 
 import os 
 import plotext as plt
+import sys
 
 
 import chessDataBase
@@ -14,26 +15,14 @@ uri = "mongodb+srv://swalha:12345678911@chess.t4z9lx3.mongodb.net/?retryWrites=t
 database = chessDataBase.ChessGameDatabase(uri)
 database.connect()
 
-game =  ChessGameReader("game.pgn")
-print(database.check_if_game_exist(game))
-print(game.get_hash())
 
-
-# for game in database.playerGames("swalha1999"):
-#     print(game)
-
-
-print("reading the game from the file")
-
-game = ChessGameReader("game.pgn")
+game = ChessGameReader(sys.argv[1])
 gameDict = game.get_headers()
 
+if database.getGames().find_one({"hash": game.get_hash()}):
+    print("The game", sys.argv[1], "is in the database")
+    exit()
 
-if database.getGames().find_one({"hash": gameDict["hash"]}):
-    print("The game is in the database")
-else:
-    database.getGames().insert_one(gameDict)
-    print("The game is not in the database")
 
 # check if the os is windows or linux
 stockfish = Stockfish("./stockfish_15_linux.bin")
@@ -44,7 +33,6 @@ stockfish.set_skill_level(20)
 
 max_eval = 2000
 board = game.board()
-evalution_list = []
 is_white = True
 
 white_player = game.headers["White"]
@@ -52,6 +40,7 @@ black_player = game.headers["Black"]
 
 white_move_accuracy = []
 black_move_accuracy = []
+evalution_list = []
 
 moves = list(game.mainline_moves())
 
@@ -73,6 +62,27 @@ for move in alive_it(moves, bar='bubbles'):
     stockfish.make_moves_from_current_position([move])
     
 
+# save the game in the database and each player average accuracy
+
+if database.getGames().find_one({"hash": gameDict["hash"]}):
+    print("The game is in the database")
+else:
+    print("The game is not in the database")
+    database.addPlayer(white_player, sum(white_move_accuracy)/len(white_move_accuracy), game.headers["Date"])
+    database.addPlayer(black_player, sum(black_move_accuracy)/len(black_move_accuracy), game.headers["Date"])
+    database.getGames().insert_one(gameDict)
+
+white_all_time_accuracy = []
+for PlayerGame in database.getPlayer(white_player):
+    white_all_time_accuracy.append(PlayerGame["accuracy"])
+
+black_all_time_accuracy = []
+for PlayerGame in database.getPlayer(black_player):
+    black_all_time_accuracy.append(PlayerGame["accuracy"])
+
+print("White player all time accuracy: ", white_all_time_accuracy)
+print("Black player all time accuracy: ", black_all_time_accuracy)
+
 print("White player: ", white_player)
 print("Black player: ", black_player)
 
@@ -84,6 +94,21 @@ plt.ylim(-max_eval, max_eval)
 plt.theme("clear")
 plt.show()
 
+plt.clear_data()
+plt.plot(white_all_time_accuracy)
+plt.title("White all time accuracy")
+plt.xlabel("Games")
+plt.ylabel("Accuracy")
+plt.theme("clear")
+plt.show()
+
+plt.clear_data()
+plt.plot(black_all_time_accuracy)
+plt.title("Black all time accuracy")
+plt.xlabel("Games")
+plt.ylabel("Accuracy")
+plt.theme("clear")
+plt.show()
 
 
 database.close()
